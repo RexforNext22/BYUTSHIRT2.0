21aaa0df35ed82693160e995434e804af5d0fe5d
Jacob Poor <jacob.poor98@gmail.com>
2021-10-28 11:27:24 -0600

    Jacob

--- BYUtShirt/RanktShirts/views.py
+++ BYUtShirt/RanktShirts/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
-print("Hello")
+from django.http import HttpResponse
+
+
+def indexPageView(request):
+    sOutput = 'Index (landing) Page'
+    return HttpResponse(sOutput)
+
+
+def AddTShirtPageView(request):
+    sOutput = 'Add T Shirt Attributes Page'
+    return HttpResponse(sOutput)
+
+
+def RankingPageView(request):
+    sOutput = 'Ranking Page'
+    return HttpResponse(sOutput)
