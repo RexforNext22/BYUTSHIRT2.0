0a3935a55ef4b271b72cdc047e12c373ec76ba2a
Supermoia24 <61605919+RexforNext22@users.noreply.github.com>
2021-12-09 12:52:53 -0700

    added by ryan

--- BYUtShirt/RanktShirts/urls.py
+++ BYUtShirt/RanktShirts/urls.py
@@ -4,11 +4,11 @@ from .views import showSingleArticlePageView
 
 urlpatterns = [
     path("", indexPageView, name="index"),
-    path("Add/", AddTShirtPageView, name="add"),
-    path("Delete/", DeleteTShirtPageView, name="delete"),
-    path("about/", AboutTShirtPageView, name="about"),
-    path("Rank/", RankingPageView, name="ranking"),
-    path("Edit/", EditPageView, name="edit"),
-    path("showSingleArticle/<int:item_id>/", showSingleArticlePageView, name="showSingleArticle") # Show an individual 
+    path("Add/", AddTShirtPageView, name="add"), # Navigate to the add page
+    path("Delete/", DeleteTShirtPageView, name="delete"), #delete
+    path("about/", AboutTShirtPageView, name="about"), # Navigate to the about page
+    path("Rank/", RankingPageView, name="ranking"), # Navigate to the ranking page
+    path("Edit/", EditPageView, name="edit"), # Navigate to the edit page
+    path("showSingleArticle/<int:item_id>/", showSingleArticlePageView, name="showSingleArticle") # Show an individual article of clothing
 ]
 
