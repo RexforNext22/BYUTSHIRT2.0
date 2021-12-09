57b8a1a16566821750304a9150ec0da8fd93329b
Jacob Poor <jacob.poor98@gmail.com>
2021-12-09 12:54:22 -0700

    jacob

--- BYUtShirt/RanktShirts/urls.py
+++ BYUtShirt/RanktShirts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import indexPageView, AddTShirtPageView, DeleteTShirtPageView, AboutTShirtPageView, RankingPageView, EditPageView
-from .views import showSingleArticlePageView
+from .views import showSingleArticlePageView, SaveTShirtPageView
 
 urlpatterns = [
     path("", indexPageView, name="index"),
@@ -9,6 +9,7 @@ urlpatterns = [
     path("about/", AboutTShirtPageView, name="about"),
     path("Rank/", RankingPageView, name="ranking"),
     path("Edit/", EditPageView, name="edit"),
-    path("showSingleArticle/<int:item_id>/", showSingleArticlePageView, name="showSingleArticle") # Show an individual 
+    path("showSingleArticle/<int:item_id>/", showSingleArticlePageView,
+         name="showSingleArticle"),  # Show an individual
+    path("save/", SaveTShirtPageView, name="save")
 ]
-
--- BYUtShirt/RanktShirts/views.py
+++ BYUtShirt/RanktShirts/views.py
@@ -11,13 +11,29 @@ from .models import Category
 from .models import Size
 from .models import ArticleOfClothing, PrimaryColor
 
+
 def indexPageView(request):
-    
+
     return render(request, 'RanktShirts/index.html')
 
 
 def AddTShirtPageView(request):
-  return render(request, 'RanktShirts/add.html')
+
+    return render(request, 'RanktShirts/add.html')
+
+
+def SaveTShirtPageView(request):
+    oArticleOfClothing = ArticleOfClothing()
+
+    oArticleOfClothing.clothing_name = request.POST['clothing_name']
+    oArticleOfClothing.price = request.POST['price']
+    oArticleOfClothing.material = request.POST['material']
+    oArticleOfClothing.category = request.POST['category']
+    oArticleOfClothing.size = request.POST['size']
+    oArticleOfClothing.primarycolor = request.POST['pColor']
+
+    oArticleOfClothing.save()
+    return render(request, 'RanktShirts/index.html')
 
 
 def DeleteTShirtPageView(request):
@@ -31,19 +47,19 @@ def AboutTShirtPageView(request):
 def RankingPageView(request):
     data = ArticleOfClothing.objects.all()
     context = {
-        "ArticleOfClothing" : data,
+        "ArticleOfClothing": data,
     }
     return render(request, 'RanktShirts/ranking.html', context)
-    
+
 
 def EditPageView(request):
     return render(request, 'RanktShirts/edit.html')
 
+
 def showSingleArticlePageView(request, item_id):
-    data = ArticleOfClothing.objects.get(id = item_id)
+    data = ArticleOfClothing.objects.get(id=item_id)
     context = {
-        "ArticleOfClothing" : data,
+        "ArticleOfClothing": data,
     }
 
     return render(request, 'RanktShirts/showItem.html', context)
-
